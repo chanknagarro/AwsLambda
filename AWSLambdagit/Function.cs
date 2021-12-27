07c23687c7f75cc357712a7a2dda81b2c3f6f940
Chandrasen Singh <chandrasen.singh@nagarro.com>
2021-12-27 16:24:29 +0530

    updated date in message

--- AWSLambdagit/Function.cs
+++ AWSLambdagit/Function.cs
@@ -21,7 +21,7 @@ namespace AWSLambdagit
         /// <returns></returns>
         public string FunctionHandler(string input, ILambdaContext context)
         {
-            return input?.ToUpper() + " Chandrasen Git-26 jan";
+            return input?.ToUpper() + " Chandrasen Git  _ Change 27 dec 2021";
         }
     }
 }
